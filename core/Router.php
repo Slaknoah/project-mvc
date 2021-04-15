04e46ab10823cd8b8472c07f020a5554144b66f9
slaknoah <slaknoah@gmail.com>
2021-04-16 00:09:49 +0600

    first commit

---
+++ core/Router.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\core;
+
+/**
+ * Class Router
+ * @package app\core
+ */
+class Router
+{
+    protected array $routes = [];
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        echo '<pre>';
+        var_dump($_SERVER);
+        echo "</pre>";
+        exit;
+    }
+}
